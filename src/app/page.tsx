67a1d3c36f8c131d918f04f35bb13f6ea3833a4e
hotrodalathasfarm-dev <hotrodalathasfarm@gmail.com>
2026-07-17 11:15:08 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
-import Experience from "@/components/experience";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import Footer from "@/components/Footer";
 
 export default function Home() {
-  return <Experience />;
+  return (
+    <div className="relative min-h-dvh bg-background">
+      <Header />
+      <Hero />
+      <Footer />
+    </div>
+  );
 }
